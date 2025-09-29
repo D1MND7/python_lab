63942da61a9c75b1d0738e8d26df500b89cc9b1f
D1MND7 <shalydima@gmail.com>
2025-09-29 11:27:45 +0300

    Update

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,7 +1,13 @@
 #1
-def min_max(mns_mxs):
-    if len(mns_mxs) != 0:
-        return print (tuple([min(mns_mxs), max(mns_mxs)]))
-    else:
-        raise ValueError
-min_max([1.5, 2, 2.0, -3.1])
+# def min_max(mns_mxs):
+#     if len(mns_mxs) != 0:
+#         return print (tuple([min(mns_mxs), max(mns_mxs)]))
+#     else:
+#         raise ValueError
+# min_max([1.5, 2, 2.0, -3.1])
+#2
+def unique_sorted(elements):
+    elements = list(set(sorted(elements)))
+    elements.sort(reverse=False)
+    return elements
+print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
