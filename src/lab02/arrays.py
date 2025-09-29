4623151831d1c3f07d0bf5c33f4eee86c80fdb29
D1MND7 <shalydima@gmail.com>
2025-09-29 11:41:34 +0300

    Update

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -6,8 +6,18 @@
 #         raise ValueError
 # min_max([1.5, 2, 2.0, -3.1])
 #2
-def unique_sorted(elements):
-    elements = list(set(sorted(elements)))
-    elements.sort(reverse=False)
-    return elements
-print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+# def unique_sorted(elements):
+#     elements = list(set(sorted(elements)))
+#     elements.sort(reverse=False)
+#     return elements
+# print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+#3
+def flatten(flatten_elem):
+    result_sort = []
+    for i in range(len(flatten_elem)):
+        if type(flatten_elem[i]) in [list, tuple]:
+            result_sort += list(flatten_elem[i])
+        else:
+            raise TypeError
+    return result_sort
+print(flatten([[1, 2], "ab"]))
