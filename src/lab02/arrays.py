c91a1e72a4f37334b8cede5dca2df28bb80f712e
D1MND7 <shalydima@gmail.com>
2025-10-03 00:08:21 +0300

    Update

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -4,12 +4,19 @@ def min_max(mns_mxs):
         return print (tuple([min(mns_mxs), max(mns_mxs)]))
     else:
         raise ValueError
+min_max([3, -1, 5, 5, 0])
+min_max([42])
+min_max([-5, -2, -9])
+min_max([])
 min_max([1.5, 2, 2.0, -3.1])
 #2
 def unique_sorted(elements):
     elements = list(set(sorted(elements)))
     elements.sort(reverse=False)
     return elements
+print(unique_sorted([3, 1, 2, 1, 3]))
+print(unique_sorted([]))
+print(unique_sorted([-1, -1, 0, 2, 2]))
 print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
 #3
 def flatten(flatten_elem):
@@ -20,4 +27,7 @@ def flatten(flatten_elem):
         else:
             raise TypeError
     return result_sort
+print(flatten([[1, 2], [3, 4]]))
+print(flatten([[1, 2], (3, 4, 5)]))
+print(flatten([[1], [], [2, 3]]))
 print(flatten([[1, 2], "ab"]))
